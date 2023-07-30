837da1f5bab9966432d540caeab10ede3c6d7d0d
MOHAMMED RAFHATH <danarafha@gmail.com>
2023-07-30 15:54:38 +0530

    aadded tocken

---
+++ src/frameworks/web/utils/generate.tokens.util.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtocken");
+const createAccessToken = (user) => {
+  return jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRATION,
+  });
+};
+const createRefreshToken = (user) => {
+  return jwt.sign({ user }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRATION,
+  });
+};
+module.exports = {
+  createAccessToken,
+  createRefreshToken,
+};
